2494087f98d7a41c3188760bd98888a255b86651
vgmiltonisaac <vgmiltonisaac@gmail.com>
2025-09-10 18:47:58 +0000

    ♻️ refactor(SplashScreen): Cambio de Splash para la app - Modificacion de app.json para los colores - Cambio de imagenes de assets/images para el slash - Configuracion en _layout.tsx del SplashScreen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,11 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
+// Previene que la pantalla de bienvenida se oculte automáticamente
+SplashScreen.preventAutoHideAsync();
+
+// Oculta la pantalla de bienvenida después de 1 segundo (1000 milisegundos)
+setTimeout(() => {
+  SplashScreen.hideAsync();
+}, 1000);
 
 export default function RootLayout() {
   return <Stack 
